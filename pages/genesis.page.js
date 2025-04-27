0de45b861d263b95cfe3c2ca36894e521e271c5c
sgsnprod <sgsnprod@gmail.com>
2025-04-27 20:21:59 +1200

    genesis

--- pages/genesis.page.js
+++ pages/genesis.page.js
@@ -1,18 +1,18 @@
-const { expect }= require('@playwright/test');
+const { expect } = require('@playwright/test');
 
-exports.GenesisPage= class GenesisPage{
-constructor(page) {
-    this.page=page;
-    this.clickEvPlans = page.getbytext('EV Plans');
-    this.openMenu=page.getbytext('Open menu');
-    this.whyGenesis=page.getbyloctor('span.flex-1 {has-text: "Why Genesis"}');
-}   
-    async goto(){
-    this.page.goto('https://genesisenergy.co.nz');
-}
+exports.GenesisPage = class GenesisPage {
+    constructor(page) {
+        this.page = page;
+        this.clickEvPlans = page.getByText('EV Plans');
+        this.openMenu = page.getByText('Open menu');
+        this.whyGenesis = page.locator('span.flex-1', { hasText: 'Why Genesis' });
+    }
 
-async openEV(){
-    this.clickEvPlans.click();
-}
+    async goto() {
+        await this.page.goto('https://genesisenergy.co.nz');
+    }
 
+    async openEV() {
+        await this.clickEvPlans.click();
+    }
 }
